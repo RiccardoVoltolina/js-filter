03cd766019b6a7692b94b8d8fe6e758c9b8d663a
RiccardoVoltolina <riky.volto@live.com>
2024-02-06 17:22:47 +0100

    change img filter with input range

--- main.js
+++ main.js
@@ -1,4 +1,14 @@
 let img = document.createElement("img");
 img.src = "./img/img-small.jpg";
 let src = document.getElementById("img-example");
+img.id = "Image";
+document.body.appendChild(img)
 src.appendChild(img);
+
+
+function ChangeFilter(event) {
+    Image = document.getElementById("Image");
+    Filter = document.getElementById("Filter").value;
+    
+    Image.style.WebkitFilter = "grayscale(" + Filter * 10 + "%)"
+}
